3017802c601187e91b68300db49335ea1d62f1f2
ednguyenn <139338857+ednguyenn@users.noreply.github.com>
2024-03-20 21:40:24 +1100

    update data ingestion

--- src/process.py
+++ src/process.py
@@ -1,20 +1,45 @@
-"""
-This is the demo code that uses hydra to access the parameters in under the directory config.
+import os 
+import sys
+from utils import CustomException, logging
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
 
-Author: Khuyen Tran
-"""
-
-import hydra
-from omegaconf import DictConfig
-
-
-@hydra.main(config_path="../config", config_name="main", version_base=None)
-def process_data(config: DictConfig):
-    """Function to process the data"""
-
-    print(f"Process data using {config.data.raw}")
-    print(f"Columns used: {config.process.use_columns}")
-
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('data/raw',"train.csv")
+    test_data_path: str=os.path.join('data/raw',"test.csv")
+    raw_data_path: str=os.path.join('data/raw',"raw.csv")
+    
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info("Entered data ingestion method or component")
+        try:
+            df = pd.read_csv('data/raw/kaggle_train.csv')
+            logging.info("Read the raw dataset as dataframe")
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            
+            df.to_csv(self.ingestion_config.raw_data_path, index=False,header=True)
+            
+            logging.info("Train test split initiated")
+            train_set, test_set=train_test_split(df,test_size=0.2, random_state=112)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+            
+            logging.info("Ingestion of the data is completed")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+            
 
 if __name__ == "__main__":
-    process_data()
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
